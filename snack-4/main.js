3fb2279d904d36d8933483120aff2e911d82051e
Giuseppe Fiore <giuseppe100891fiore@gmail.com>
2026-02-05 12:51:41 +0100

    commit snack 4

--- snack-4/main.js
+++ snack-4/main.js
@@ -1,2 +1,16 @@
 // Snack 4
-//
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+const numeriDispari = [];
+
+for (let i = 0; i < 6; i++) {
+    const numero = parseInt(prompt(`Inserisci il numero ${i + 1}`));
+
+    if (numero % 2 !== 0) {
+    numeriDispari.push(numero);
+    }
+}
+
+console.log(numeriDispari);
